fbfa96525cb401979769a3fcd5642181143dcd47
Mark <m_he@uncg.edu>
2020-05-21 19:35:11 -0400

    First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * author: Mark H.
+ * date: 05/19/20
+ * purpose: To create a simple Print statement and upload it with Git.
+ */
+
+public class HelloWorld{
+    
+    public static void main(String argv[]){
+        System.out.print("Hello World!");
+    }
+}
