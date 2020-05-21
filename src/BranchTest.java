a29cd168ab0ed0cd9f0a5da19528ebbae2fc1b6a
Mark <m_he@uncg.edu>
2020-05-21 19:46:42 -0400

    New branch and file commit

---
+++ src/BranchTest.java
@@ -0,0 +1,12 @@
+/**
+ * author: Mark H.
+ * date: 05/21/20
+ * purpose: To create a new branch and push to masters.
+ */
+
+public class BranchTest{
+    
+    public static void main(String argv[]){
+        System.out.print("Hello master!");
+    }
+}
